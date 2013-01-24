516010cd8f1dfae7730ce8b57e8907c20cf871ed
Yuriy Goldshtrakh <yuriy@yuriy-VirtualBox.(none)>
2013-01-24 13:42:05 -0800

    simplified code

--- Rakefile.rb
+++ Rakefile.rb
@@ -15,37 +15,39 @@ task :import_from_csv do
 	customers = Stripe::Customer.all
 
 	csv.each do |row|
+		begin
 		exp_month = row[18][0..1]
 		exp_year = row[18][2..3]
 		customer_name = row[1] ? row[1] : row[2]
 		description = row[0] + "-" + customer_name + "-" + row[3]
-		descriptions = customers.collect{|c| c.description}
-
-		if descriptions.include?(description)
+		if customer = customers[:data].find{|customer| customer[:description]==description}
 			existing_customers_count += 1
 		else
-			customer = Stripe::Customer.create({
-				:description => description,
-        :card => {
-          :number => row[19],
-          :exp_month => exp_month,
-          :exp_year => exp_year,
-          :name => row[2],
-	        :cvc => row[7]
-        }
-      })
-			added_customers_count += 1
-
-			new_customer = Stripe::Customer.retrieve(customer.id)
-			company_name = new_customer[:description].split("-")[0]
-			user_name = new_customer[:description].split("-")[1]
-			card_type = new_customer[:description].split("-")[2]
-			last4 = new_customer[:active_card][:last4]
-
-			CSV.open("stripe_customers.csv", "ab") do |a|
-				a << [company_name, user_name, card_type, last4, new_customer[:id]]
-			end
+				customer = Stripe::Customer.create({
+					:description => description,
+	        		:card => {
+	          			:number => row[19],
+	          			:exp_month => exp_month,
+	          			:exp_year => exp_year,
+	          			:name => row[2],
+		        		:cvc => row[7]
+	        		}
+	      		})
+	      		added_customers_count += 1
 		end
+		p customer[:id]
+		rescue
+			p "failed on #{row}"
+		end
+
+		company_name = customer[:description].split("-")[0]
+		user_name = customer[:description].split("-")[1]
+		card_type = customer[:description].split("-")[2]
+		last4 = customer[:active_card][:last4]
+		CSV.open("stripe_customers.csv", "ab") do |a|
+			a << [company_name, user_name, card_type, last4, new_customer[:id]]
+		end
+			
 	end
 	puts "Skipped #{existing_customers_count} already existing customer#{"s" if existing_customers_count != 1}. Added #{added_customers_count} new customer#{"s" if added_customers_count != 1}!"
 end
