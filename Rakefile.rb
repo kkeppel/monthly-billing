09e4a2deeaa51b9da5abcd919cbd9a6543af2b3b
= <kathy@cater2.me>
2013-01-24 15:31:49 -0500

    append customer information to stripe customer list csv upon creation

--- Rakefile.rb
+++ Rakefile.rb
@@ -9,30 +9,42 @@ added_customers_count = 0
 desc "Import CSV credit card information in Stripe"
 
 task :import_from_csv do
-	file = File.read('cards.csv')
+	file = File.read('credit_card_payments_workbook.csv')
 	csv = CSV.parse(file, :headers => true)
 
 	customers = Stripe::Customer.all
 
 	csv.each do |row|
-		exp_month = row[2][0..1]
-		exp_year = row[2][2..3]
-		description = row[0]
+		exp_month = row[18][0..1]
+		exp_year = row[18][2..3]
+		customer_name = row[1] ? row[1] : row[2]
+		description = row[0] + "-" + customer_name + "-" + row[3]
 		descriptions = customers.collect{|c| c.description}
 
 		if descriptions.include?(description)
 			existing_customers_count += 1
 		else
-			Stripe::Customer.create({
-        :description => row[0],
+			customer = Stripe::Customer.create({
+				:description => description,
         :card => {
-          :number => row[1],
+          :number => row[19],
           :exp_month => exp_month,
           :exp_year => exp_year,
-          :name => row[3]
+          :name => row[2],
+	        :cvc => row[7]
         }
       })
 			added_customers_count += 1
+
+			new_customer = Stripe::Customer.retrieve(customer.id)
+			company_name = new_customer[:description].split("-")[0]
+			user_name = new_customer[:description].split("-")[1]
+			card_type = new_customer[:description].split("-")[2]
+			last4 = new_customer[:active_card][:last4]
+
+			CSV.open("stripe_customers.csv", "ab") do |a|
+				a << [company_name, user_name, card_type, last4, new_customer[:id]]
+			end
 		end
 	end
 	puts "Skipped #{existing_customers_count} already existing customer#{"s" if existing_customers_count != 1}. Added #{added_customers_count} new customer#{"s" if added_customers_count != 1}!"
