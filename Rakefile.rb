29251560ed18f1a498dfce220699d09699942631
Kathy <kathykeppel@gmail.com>
2013-03-04 19:48:00 -0500

    more refactoring

--- Rakefile.rb
+++ Rakefile.rb
@@ -11,6 +11,8 @@ existing_customers_count = 0
 added_customers_count = 0
 desc "Import CSV credit card information in Stripe"
 
+### -----------------------------------
+
 def read_csv(csv)
 	file = File.read(csv)
 	CSV.parse(file, :headers => true)
@@ -18,16 +20,37 @@ end
 
 ### -----------------------------------
 
-def loop_through_stripe
-	customers=[]
-	count=100
-	offset=0
-	until count<100
-		stripe_request = Stripe::Customer.all(count: count,offset: offset)
-		customers.concat stripe_request.data
-		count = stripe_request.data.count
-		offset+=count
+def append_to_successful_charges_csv(customer_id, amount, charge_id, paid, refunded, created_at)
+	CSV.open("successful_charges.csv", "ab") do |a|
+		a << [customer_id, amount, charge_id, paid, refunded, created_at]
 	end
+end
+
+### -----------------------------------
+
+def append_to_stripe_customers_csv(company_name, user_name, card_type, las4, order_id, customer_id)
+	CSV.open("stripe_customers.csv", "ab") do |a|
+		a << [company_name, user_name, card_type, last4, order_id, customer_id]
+	end
+end
+
+### -----------------------------------
+
+def create_customer(description, number, exp_month, exp_year, name, cvc)
+	Stripe::Customer.create({
+		:description => description,
+		:card => {
+			:number => number,
+			:exp_month => exp_month,
+			:exp_year => exp_year,
+			:name => name,
+			:cvc => cvc
+		}
+	})
+end
+
+def charge_customer(customer, amount)
+	Stripe::Charge.create(:customer => customer, :amount => amount, :currency => "usd")
 end
 
 ### -----------------------------------
@@ -78,7 +101,15 @@ end
 
 task :remove_all_customers do
 	all_removed_customers = 0
-	customers = loop_through_stripe
+	customers=[]
+	count=100
+	offset=0
+	until count<100
+		stripe_request = Stripe::Customer.all(count: count,offset: offset)
+		customers.concat stripe_request.data
+		count = stripe_request.data.count
+		offset+=count
+	end
 	customers.each do |c|
 		customer = Stripe::Customer.retrieve(c.id)
 		customer.delete
@@ -95,7 +126,15 @@ end
 
 task :import_from_csv do
 	csv = read_csv('cc_import_workbook_AL.csv')
-	customers = loop_through_stripe
+	customers=[]
+	count=100
+	offset=0
+	until count<100
+		stripe_request = Stripe::Customer.all(count: count,offset: offset)
+		customers.concat stripe_request.data
+		count = stripe_request.data.count
+		offset+=count
+	end
 	csv.each do |row|
 		begin
 			exp_month = row[5][0..1]
@@ -105,26 +144,9 @@ task :import_from_csv do
 			if customer = customers.find{|customer| customer[:description]==description}
 				existing_customers_count += 1
 			else
-				customer = Stripe::Customer.create({
-				 :description => description,
-				 :card => {
-				   :number => row[6],
-				   :exp_month => exp_month,
-				   :exp_year => exp_year,
-				   :name => row[2],
-				   :cvc => row[4]
-				 }
-				})
+				customer = create_customer(description, row[6], exp_month, exp_year, row[2], row[4])
 				added_customers_count += 1
-
-				company_name = row[0]
-				user_name = customer_name
-				card_type = row[3]
-				last4 = customer[:active_card][:last4]
-				order_id = customer[:description]
-				CSV.open("stripe_customers.csv", "ab") do |a|
-					a << [company_name, user_name, card_type, last4, order_id, customer[:id]]
-				end
+				append_to_stripe_customers_csv(row[0], customer_name, row[3], customer[:active_card][:last4], customer[:description], customer[:id])
 			end
 			p customer[:description]
 		rescue Stripe::CardError => e
@@ -149,23 +171,8 @@ end
 
 task :charge_customer, :customer_id, :amount do |t, args|
 	puts "Args were: #{args}\nCustomer_id = #{args[:customer_id]}\nAmount = #{args[:amount]}"
-
-	charge = Stripe::Charge.create(
-		:amount => args[:amount],
-		:currency => "usd",
-		:customer => args[:customer_id]
-	)
-
-	customer_id = args[:customer_id]
-	amount = args[:amount]
-	charge_id = charge[:id]
-	paid = charge[:paid]
-	refunded = charge[:refunded]
-	created_at = charge[:created]
-
-	CSV.open("successful_charges.csv", "ab") do |a|
-		a << [customer_id, amount, charge_id, paid, refunded, created_at]
-	end
+	charge = charge_customer(args[:customer_id], args[:amount])
+	append_to_successful_charges_csv(args[:customer_id], args[:amount], args[:charge_id], charge[:paid], charge[:refunded], charge[:created]) 
 end
 
 ### -----------------------------------
@@ -173,28 +180,11 @@ end
 task :charge_customers do
 	csv = read_csv('charges.csv')
   charges = 0
-
 	csv.each do |row|
 		begin
-			charge = Stripe::Charge.create(
-				:customer => row[0],
-				:amount => row[1],
-				:currency => "usd"
-			)
-
-			customer_id = row[0]
-			amount = row[1]
-			charge_id = charge[:id]
-			paid = charge[:paid]
-			refunded = charge[:refunded]
-			created_at = charge[:created]
-
-			CSV.open("successful_charges.csv", "ab") do |a|
-				a << [customer_id, amount, charge_id, paid, refunded, created_at]
-			end
-
+			charge = charge_customer(row[0], row[1])
+			append_to_successful_charges_csv(row[0], row[1], charge[:id], charge[:paid], charge[:refunded], charge[:created])
 			charges += 1
-
 			puts "charged customer #{row[0]} #{amount}"
 		rescue Stripe::CardError => e
 			card_error(e, row)
@@ -211,7 +201,6 @@ task :charge_customers do
 			p "failed on #{row}"
 		end
 	end
-
 	puts "Charged #{charges} customer#{"s" if charges != 1}!"
 end
 
@@ -220,23 +209,11 @@ end
 task :refund_charges do
 	csv = read_csv('refund_charges.csv')
   refunds = 0
-
 	csv.each do |row|
 		begin
 			charge = Stripe::Charge.retrieve(row[0])
-			charge.refund 
-
-			customer_id = charge[:customer]
-			amount = row[1]
-			charge_id = charge[:id]
-			paid = charge[:paid]
-			refunded = charge[:refunded]
-			created_at = charge[:created]
-
-			CSV.open("successful_charges.csv", "ab") do |a|
-				a << [customer_id, amount, charge_id, paid, refunded, created_at]
-			end
-
+			charge.refund
+			append_to_successful_charges_csv(charge[:customer], charge[:amount], row[0], charge[:paid], charge[:refunded], charge[:created]) 
 			charges += 1
 			puts "charged customer #{row[0]} #{amount}"
 		rescue Stripe::CardError => e
@@ -254,58 +231,38 @@ task :refund_charges do
 			p "failed on #{row}"
 		end
 	end
-
 	puts "Refunded #{refunds} customer#{"s" if refunds != 1}!"	
 end
 
+### -----------------------------------
+
 task :refund_charge, :charge_id do |t, args|
 	puts "Args were: #{args}\charge_id = #{args[:charge_id]}"
-
 	charge = Stripe::Charge.retrieve(args[:charge_id])
 	charge.refund
-
-	customer_id = charge[:customer]
-	amount = charge[:amount]
-	charge_id = args[:charge_id]
-	paid = charge[:paid]
-	refunded = charge[:refunded]
-	created_at = charge[:created]
-
-	CSV.open("successful_charges.csv", "ab") do |a|
-		a << [customer_id, amount, charge_id, paid, refunded, created_at]
-	end
+	append_to_successful_charges_csv(charge[:customer], charge[:amount], args[:charge_id], charge[:paid], charge[:refunded], charge[:created]) 
 end
 
 ### -----------------------------------
 
 task :add_customer, :number, :exp_month, :exp_year, :name, :cvc, :company_name, :card_type, :city do |t, args|
 	puts "\ncity: #{args[:city]}\nNumber: #{args[:number]}\nExp Month: #{args[:exp_month]}\nExp Year: #{args[:exp_year]}\nName: #{args[:name]}\nCVC: #{args[:cvc]}\nCompany Name: #{args[:company_name]}\nCard Type: #{args[:card_type]}"
-
-	customers = loop_through_stripe
+	customers=[]
+	count=100
+	offset=0
+	until count<100
+		stripe_request = Stripe::Customer.all(count: count,offset: offset)
+		customers.concat stripe_request.data
+		count = stripe_request.data.count
+		offset+=count
+	end
 	begin
 		description = args[:company_name] + "-" + args[:name] + "-" + args[:city] + "-" + args[:number].to_s[-4,4] 
 		if customer = customers.find{|customer| customer[:description]==description}
 			puts "This customer already exists in Stripe."
 		else
-			customer = Stripe::Customer.create({
-	       :description => description,
-	       :card => {
-	         :number => args[:number],
-	         :exp_month => args[:exp_month],
-	         :exp_year => args[:exp_year],
-	         :name => args[:name],
-	         :cvc => args[:cvc]
-	       }
-	     })
-			# add new customer to stripe_customers.csv
-			company_name = args[:company_name]
-			user_name = args[:name]
-			card_type = args[:card_type]
-			last4 = customer[:active_card][:last4]
-			order_id = customer[:description]
-			CSV.open("stripe_customers.csv", "ab") do |a|
-				a << [company_name, user_name, card_type, last4, order_id, customer[:id]]
-			end
+			customer = create_customer(description, args[:number], args[:exp_month], args[:exp_year], args[:name], args[:cvc])
+			append_to_stripe_customers_csv(args[:company_name], args[:name], args[:card_type], customer[:active_card][:last4], customer[:description], customer[:id])
 			puts "Added customer #{customer[:description]}"
 		end
 	rescue Stripe::CardError => e
